a88ee806412be86f7762f12e457ea81db5246c7e
xiexiaopeng2002 <2020130293@mail.buct.edu.cn>
2024-04-25 10:54:01 +0800

    selfhost php

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import sys
 from PySide6 import QtWidgets
 from pagecontroller import Controller
-
+import subprocess
 import rc_lmegui
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     controller = Controller()
     controller.show_mainwindow()
+    php_path = "D:\\php8\\php.exe"
+    subprocess.Popen([php_path, "-S", "localhost:8000", "-t", "D:\\Documents\\Qt\\resources\\3rdparty\\php"])
     sys.exit(app.exec())
